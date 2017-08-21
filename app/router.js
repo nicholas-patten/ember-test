4e7a80ecfb1b6eb946c5bd25dc81550a5e07a9e5
Nicholas Patten <ned2001@gmail.com>
2017-08-21 12:53:25 +0100

    Feature/new template (#1)
    * Generated Application Template
    
    * Generated New Route 'scientists'
    
    * Added new routes and javascript templates
    
    * Added new routes and javascript templates
    
    * Added missing files

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('scientists');
+  this.route('programmers');
+  this.route('builders');
+  this.route('manks');
 });
 
 export default Router;
